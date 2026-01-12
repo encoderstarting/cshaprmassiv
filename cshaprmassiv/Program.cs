67a179c06cb6cb323913656a2d20d2ff6357620b
encoderstarting <112802133+encoderstarting@users.noreply.github.com>
2026-01-13 01:23:24 +0300

    Добавьте файлы проекта.

---
+++ cshaprmassiv/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Сколько хотите элементов в массиве? Введите ниже...");
+if (!int.TryParse(Console.ReadLine(), out int value))
+{
+    Console.WriteLine("Введите значение!");
+    return;
+}
+
+int[] myArray = new int[value];
+for(int i = 0;i < myArray.Length; i++)
+{
+    Console.WriteLine($"Введите элемент массива под индексом {i}:\t ");
+    myArray[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Вывод массива: ");
+for(int i = 0; i < myArray.Length; i++)
+{
+    Console.WriteLine(myArray[i]);
+
+}
