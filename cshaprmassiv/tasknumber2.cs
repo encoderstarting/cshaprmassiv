c7fba451dddbfb1bec0312f19281a6b8b6fff299
encoderstarting <112802133+encoderstarting@users.noreply.github.com>
2026-01-13 01:29:15 +0300

    Добавление ошибки

---
+++ cshaprmassiv/tasknumber2.cs
@@ -0,0 +1,19 @@
+﻿public class tasknumber2
+{
+    public static void Run()
+    {
+        Console.WriteLine("Введите сколько элементов должно быть в массиве: ");
+        if (!int.TryParse(Console.ReadLine(), out int chouse))
+        {
+            Console.WriteLine("Введите значение!");
+        }
+
+
+
+
+
+
+
+
+    }
+}
