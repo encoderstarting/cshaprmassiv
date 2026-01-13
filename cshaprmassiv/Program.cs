e182c22a7be559366ea761073d4bc2391dcd87cb
encoderstarting <112802133+encoderstarting@users.noreply.github.com>
2026-01-14 00:28:28 +0300

    Добавление кейсов

--- cshaprmassiv/Program.cs
+++ cshaprmassiv/Program.cs
@@ -1,6 +1,20 @@
 ﻿while (true)
 {
     Console.WriteLine("\nМеню: ");
+    Console.WriteLine("1 - Вывести массив с клавиатуры самостоятельно");
+    Console.WriteLine("2 - Вывести массив в обратном порядке");
+    Console.WriteLine("3 - Вывести сумму четных элемнтов массива");
+    Console.WriteLine("4  - Вывести наименьший элемент массива");
+    Console.WriteLine("0 - выход");
+    string choise = Console.ReadLine();
+    switch (choise)
+    {
+        case "1":
+            Console.WriteLine("Ввод масива...");
+            break;
+        case "2": Console.WriteLine("Вывод массива в обратном порядке..."); break;
+    }
+ 
 }
 
 
