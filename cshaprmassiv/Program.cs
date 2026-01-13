930b45b9323b7adbd8ceeb662767f9d293bdbef8
encoderstarting <112802133+encoderstarting@users.noreply.github.com>
2026-01-14 00:22:56 +0300

    Добавление меню в program

--- cshaprmassiv/Program.cs
+++ cshaprmassiv/Program.cs
@@ -1,34 +1,13 @@
-﻿
-
-
-
-
-
-
-
-
-
-
-Console.WriteLine("Введите сколько элементов должно быть в массиве: ");
-if (!int.TryParse(Console.ReadLine(), out int chouse))
+﻿while (true)
 {
-    Console.WriteLine("Введите значение!");
+    Console.WriteLine("\nМеню: ");
 }
-int[] myLine = new int[chouse];
-for (int i = 0; i < myLine.Length; i++)
-{
-    Console.WriteLine($"Введите элемент массива под индексом {i}:\t ");
-    myLine[i] = int.Parse(Console.ReadLine());
-}
-int min = myLine[0];
-Console.WriteLine("Вывод наименьшего числа в массиве: ");
-for(int i=1; i < myLine.Length; i++)
-{
 
-    if (min > myLine[i])
-    {
-        min = myLine[i];
-    }
 
-}
-Console.WriteLine(min);
+
+
+
+
+
+
+
